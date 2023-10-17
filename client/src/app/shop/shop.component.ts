439b2bcdf2722f18c94afe544c70c320005e65b4
Nguyễn Lê Nghĩa (ЦИТиС) <nghianl.ts@vietsov.com.vn>
2023-10-17 15:51:00 +0700

    94th commit

---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,60 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../share/models/product';
+import { ShopService } from './shop.service';
+import { Brand } from '../share/models/brand';
+import { Type } from '../share/models/type';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+  products: Product[] = [];
+  brands: Brand[] = [];
+  types: Type[] = [];
+  brandIdSelected = 0;
+  typeIdSelected = 0;
+
+  constructor(private shopService: ShopService) {
+  }
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(){
+    this.shopService.getProducts().subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error)
+    })
+  }
+
+  getBrands(){
+    this.shopService.getBrands().subscribe({
+      next: response => this.brands = [{id: 0, name: 'All'}, ...response],
+      error: error => console.log(error)
+    })
+  }
+
+  getTypes(){
+    this.shopService.getTypes().subscribe({
+      next: response => this.types = [{id: 0, name: 'All'}, ...response],
+      error: error => console.log(error)
+    })
+  }
+
+  onBrandSelected(brandId: number){
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number){
+    this.typeIdSelected = typeId;
+    this.getProducts();
+  }
+
+}
---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,33 @@
+
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../share/models/product';
+import { Pagination } from '../share/models/pagination';
+import { Brand } from '../share/models/brand';
+import { Type } from '../share/models/type';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+    // add a parameter has name "brandId" ,value "brandId"
+    if (brandId) params = params.append('brandId', brandId);
+    if (typeId) params = params.append('typeId', typeId);
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products',{params});
+  }
+
+  getBrands() {
+    return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<Type[]>(this.baseUrl + 'products/types');
+  }
+}
